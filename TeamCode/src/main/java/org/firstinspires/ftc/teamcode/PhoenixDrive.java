ebc02f1c19aaaf840ede893a2c408eef37e6d3e8
Tasbeet <tasbeeti@gmail.com>
2020-01-01 15:44:55 -0800

    Tele-Op for interleague

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PhoenixDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PhoenixDrive.java
@@ -11,11 +11,12 @@ public class PhoenixDrive extends LinearOpMode {
 
     private DcMotor right_drive;
     private DcMotor left_drive;
-    private DcMotor arm;
+    private DcMotor intake1;
+    private DcMotor intake2;
 
     private Servo grab_right;
     private Servo grab_left;
-    private Servo intake;
+
 
     /**
      * This function is executed when this Op Mode is selected from the Driver Station.
@@ -24,19 +25,17 @@ public class PhoenixDrive extends LinearOpMode {
     public void runOpMode() {
         right_drive = hardwareMap.dcMotor.get("right_drive");
         left_drive = hardwareMap.dcMotor.get("left_drive");
-        arm = hardwareMap.dcMotor.get("arm");
+        intake1 = hardwareMap.dcMotor.get("intake1");
+        intake2 = hardwareMap.dcMotor.get("intake2");
 
         grab_right = hardwareMap.servo.get("grab_right");
         grab_left = hardwareMap.servo.get("grab_left");
 
         grab_left.setDirection(Servo.Direction.REVERSE);
 
-        intake = hardwareMap.servo.get("intake");
-
         // Reverse one of the drive motors.
-        left_drive.setDirection(DcMotorSimple.Direction.REVERSE);
-        // Arm motor is also installed reverse
-        arm.setDirection(DcMotorSimple.Direction.REVERSE);
+        right_drive.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         waitForStart();
         if (opModeIsActive()) {
@@ -46,17 +45,27 @@ public class PhoenixDrive extends LinearOpMode {
                 // The Y axis of a joystick ranges from -1 in its topmost position
                 // to +1 in its bottommost position. We negate this value so that
                 // the topmost position corresponds to maximum forward power.
-                left_drive.setPower(-gamepad1.right_stick_y + gamepad1.right_stick_x);
-                right_drive.setPower(-gamepad1.right_stick_y - gamepad1.right_stick_x);
+                left_drive.setPower(gamepad1.right_stick_y - gamepad1.right_stick_x);
+                right_drive.setPower(gamepad1.right_stick_y + gamepad1.right_stick_x);
 
-                double armPower = -gamepad2.right_stick_y;
-                if (!gamepad2.left_bumper) {
-                    armPower /= 2;
+                double outakePow = gamepad2.left_trigger;
+                double intakePow = gamepad2.right_trigger;
+
+                if (intakePow > 0) {
+                    intake1.setPower(intakePow);
+                } else if (gamepad2.right_bumper){
+                    intake1.setPower(-1);
+                } else {
+                    intake1.setPower(0.0);
                 }
-                arm.setPower(armPower);
 
-                double intakePosition = (gamepad2.left_stick_y + 1) / 2;
-                intake.setPosition(intakePosition);
+                if (outakePow > 0) {
+                    intake2.setPower(outakePow);
+                } else if (gamepad2.left_bumper){
+                    intake2.setPower(-1);
+                } else {
+                    intake2.setPower(0.0);
+                }
 
                 if (gamepad2.dpad_up){
                     grab_left.setPosition(1);
@@ -68,8 +77,6 @@ public class PhoenixDrive extends LinearOpMode {
 
                 telemetry.addData("leftMotor", "%d, power: %.2f", left_drive.getCurrentPosition(), left_drive.getPower());
                 telemetry.addData("rightMotor", "%d, power: %.2f", right_drive.getCurrentPosition(), right_drive.getPower());
-                telemetry.addData("Arm Pow", arm.getPower());
-                telemetry.addData("Intake Pos", intakePosition);
                 telemetry.addData("Grabber Pos", grab_left.getPosition());
                 telemetry.update();
             }
